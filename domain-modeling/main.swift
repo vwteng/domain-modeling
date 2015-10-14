1ceca8118f6a066a6cb954fc1ee38da0f013d5cb
Vivian <vwteng@uw.edu>
2015-10-14 14:07:27 -0700

    Initial Commit

---
+++ domain-modeling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  domain-modeling
+//
+//  Created by Vivian on 10/14/15.
+//  Copyright © 2015 Vivian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
